544ea02c4b67651b5e94ba949ce4740721333ea5
Jose Bustamante <jose.bustamante@Joses-MacBook-Pro.local>
2017-02-28 15:25:28 +0100

    languages less dutch

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+  // Mala idea, porque cuando entra a index te redirecciona al lenguage de tu navegador aunque tu no quieras.
+  // if(navigator.language.includes('en') && !window.location.href.includes('index-uk.html')){
+  //   window.location.href = "index-uk.html";
+  // } else if (navigator.language.includes('de') && !window.location.href.includes('index-ger.html')) {
+  //     window.location.href = "index-ger.html";
+  // }
+
   var win = $(this);
 
   if (win.width() < 768) {
